1729282e402593fd42db3d31b1a6f0ec8088d031
ardnew <andrew@ardnew.com>
2020-03-08 00:30:21 -0600

    doc updates

--- example_test.go
+++ example_test.go
@@ -6,18 +6,20 @@ import (
 	"github.com/ardnew/ft232h"
 )
 
-// Example demonstrates opening an FT232H with optional command line flags that
-// specify which FTDI device to use if more than one is connected to the system.
-//
-// If no flags are provided, the first MPSSE-capable device found is used.
-// Use -h to see all available flags.
-//
-// See the NewFT232H() godoc for other semantics related to the flag package.
-//
-// To open a specific device without using command line flags, use one of the
-// functions of form NewFT232HWith*(). In particular, NewFT232HWithMask(nil)
-// will open the first compatible device found.
-func Example() {
+func Example_command_line() {
+
+	// Call NewFT232H() to open an FT232H device from a command line-oriented
+	// application to help select which FTDI device to use (by parsing predefined
+	// command line flags) if more than one is connected to the system.
+	//
+	// If no flags are provided, the first MPSSE-capable USB device found is used.
+	// Use -h to see all available flags.
+	//
+	// See the NewFT232H() godoc for other semantics related to the flag package.
+	//
+	// To open a specific device without using command line flags, use one of the
+	// functions of form NewFT232HWith*(). In particular, NewFT232HWithMask(nil)
+	// will open the first compatible device found.
 
 	// Open first device that matches all command line flags (if any provided)
 	ft, err := ft232h.NewFT232H()
@@ -28,11 +30,5 @@ func Example() {
 
 	// At this point, you can call Init() or Config() on one of the interface
 	// fields GPIO, SPI, I2C, ...
-	doStuff(ft)
-}
-
-func doStuff(ft *ft232h.FT232H) {
-
-	// FT232H implements String() descriptively
-	log.Printf("using: %s", ft)
+	log.Printf("using: %s", ft) // FT232H implements String() descriptively
 }
